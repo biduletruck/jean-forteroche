257317bfb46e92687d82d5998e955319b8aea5d5
clement yann <clementyann@free.fr>
2017-05-04 12:53:58 +0200

    Update ViewAccueil.php

--- Web/Accueil/View/ViewAccueil.php
+++ Web/Accueil/View/ViewAccueil.php
@@ -1,6 +1,6 @@
 <div class="container-fluid">
-    <div class="row">
-        <div class="col-md-12">
+        <div class="row ">
+            <div class="col-xs-12 col-md-12 ">
             <p> <?= $accueil->art_content; ?></p>
         </div>
     </div>
