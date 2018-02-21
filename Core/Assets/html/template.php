3c0ffaf56b424dce9509881ea380e7aa9a534f6c
biduletruck <clementyann@free.fr>
2018-02-21 22:15:38 +0100

    mise a jour js

--- Core/Assets/html/template.php
+++ Core/Assets/html/template.php
@@ -8,7 +8,7 @@
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <<script
+    <script
             src="https://code.jquery.com/jquery-1.12.4.min.js"
             integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
             crossorigin="anonymous"></script>
